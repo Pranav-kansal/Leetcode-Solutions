fe946067102aeeda9f7fb5740de2723a4df87026
Pranav-kansal <91832841+Pranav-kansal@users.noreply.github.com>
2022-11-24 00:15:19 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (82.78%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool wordPattern(string p, string s) {
+        map<char,string> mp;
+        map<string,char> ms;
+        int j=0;
+        for(int i=0;i<p.size();i++){
+            string temp="";
+            while(j<s.size() && s[j]!=' '){   
+                temp+=s[j++];
+            }
+            j++;                               
+            if(temp.empty()||(mp.find(p[i])!=mp.end()||ms.find(temp)!=ms.end()) && (mp[p[i]]!=temp|| ms[temp]!=p[i])) 
+                return false;
+            mp[p[i]]=temp;
+            ms[temp]=p[i];
+            temp="";                           
+        }
+        if(j-1<s.size()) return false;        
+        
+        return true;
+    }
+};
